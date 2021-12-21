4acf685fc2c92ad88f7b980aed0e5bf57f0add7a
ITheMexicanI <63707304+ITheMexicanI@users.noreply.github.com>
2021-12-21 11:33:28 +0300

    Add files via upload

---
+++ Programming/src/lab4/exceptions/InvalidCommandException.java
@@ -0,0 +1,7 @@
+package lab4.exceptions;
+
+public class InvalidCommandException extends Exception {
+    public InvalidCommandException(String message) {
+        super(message);
+    }
+}
---
+++ Programming/src/lab4/exceptions/OrganismCreateException.java
@@ -0,0 +1,7 @@
+package lab4.exceptions;
+
+public class OrganismCreateException extends RuntimeException {
+    public OrganismCreateException(String message) {
+        super(message);
+    }
+}
